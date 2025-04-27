b45ae4099edbd655b6d4537313c8d66a7490a4c6
Amam Rachmanto <amam11rachmanto@gmail.com>
2025-04-27 11:22:29 +0700

    update function say_hello

--- lib/coba.dart
+++ lib/coba.dart
@@ -2,5 +2,6 @@ library dart_packages;
 
 export 'src/say_hello.dart'; //digunakan untuk mengeksport seluruh isi dari file say_hello.dart
 export 'src/customer.dart' show Customer, Product, Category; //digunakan untuk megeksport beberapa class/isi dari file customer.dart
+export 'src/perhitungan.dart';
 
 
